8561e61b1dcf90f7985bc5fe387e1fc142cc5fec
nozo <nozomi.pine@gmail.com>
2018-01-16 23:24:23 +0900

    Add MutationObserver.

--- content.js
+++ content.js
@@ -1,5 +1,27 @@
 console.log("content.js is loaded!");
 
+function spawn_observer(target, option){
+    if(!target) return null;
+
+    console.log(target);
+
+    const obs = new MutationObserver((rec) => {
+	console.log(rec);
+    });
+    
+    obs.observe(target, option);
+
+    return obs;
+}
+
+function get_dom_from_page(query){
+    return document.querySelector(query);
+}
+
 chrome.runtime.onMessage.addListener(
-    (request, sender, sendResponse) => {console.log("res":request.msg)}
+    (request, sender, sendResponse) => {
+	const query = request.msg;
+	console.log("res:"+query);
+	spawn_observer(get_dom_from_page(query),{childList: true});
+    }
 );
