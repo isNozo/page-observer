8b846b575ee3265d319000850a148ed293d6b825
nozo <nozomi.pine@gmail.com>
2018-01-17 18:58:43 +0900

    Move notification to popup.js.

--- content.js
+++ content.js
@@ -1,47 +1,31 @@
-console.log("content.js is loaded!");
+function send_msg_from_content(data){
+    chrome.runtime.sendMessage(
+	{from: "content", msg: data},
+	(response) => {}
+    );
+}
 
 function spawn_observer(query, option){
-    const target = get_dom_from_page(query);
+    const target = document.querySelector(query);
     
     if(!target) return null;
     console.log("find dom : ", target);
     
     const obs = new MutationObserver((rec) => {
 	console.log(rec);
+	send_msg_from_content(rec[0].addedNodes[0].innerText);
     });
     
     obs.observe(target, option);
-    console.log("spawn observer!");
 
     return obs;
 }
 
-function get_dom_from_page(query){
-    return document.querySelector(query);
-}
-
-function send_notification(msg){
-    chrome.notifications.create({
-	type: "basic",
-	title: "Page Observer",
-	message: msg,
-	iconUrl: "dummy_icon.png"
-    });
-}
-
-function get_data_from_rec(rec){
-    const dom = rec[0].addedNodes[0];
-    if(!dom){
-	return dom.innerText;
-    }else{
-	return "";
-    }
-}
-
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
+	if(request.from != "extension") return;
+	
 	const query = request.msg;
-	console.log("res:"+query);
-	spawn_observer(query, {childList: true});
+	spawn_observer(query, {childList:true});
     }
 );
--- popup.js
+++ popup.js
@@ -1,19 +1,33 @@
-console.log("popup.js is loaded!");
-
-function send(data){
+function send_msg_from_extension(data){
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
 	chrome.tabs.sendMessage(
 	    tabs[0].id,
-	    {msg: data},
-	    (res) => {}
+	    {from:"extension", msg: data},
+	    (response) => {}
 	);
     });
 }
 
+function notify(msg){
+    chrome.notifications.create({
+	type: "basic",
+	title: "Page Observer",
+	message: msg,
+	iconUrl: "dummy_icon.png"
+    });
+}
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+	if(request.from != "content") return;
+	console.log("popup received :", request.msg);
+	notify(request.msg);
+    }
+);
+
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#rq_button').addEventListener('click', () => {
 	const query = document.querySelector('#query_input').value;
-	console.log("snd:"+query);
-	send(query);
+	send_msg_from_extension(query);
     });
 });
