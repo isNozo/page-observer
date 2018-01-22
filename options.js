1c3ffe0905fc7849c527e8c2b352473bd1fef69c
nozo <nozomi.pine@gmail.com>
2018-01-22 16:43:32 +0900

    Modify option page to show config by input node.

--- options.js
+++ options.js
@@ -1,14 +1,20 @@
-function create_prop_field(target){
-    const div = document.createElement("div");
-    const field = `
-    Query: ${target.query}
-    Title: ${target.title.query}
-    Body: ${target.body.query}
-    Icon: ${target.icon.query}
-    Image: ${target.image.query}
-    `
-    div.appendChild(document.createTextNode(field));
-    return div;
+function create_form(id, value){
+    const input_field = document.createElement("div");
+    input_field.classList.add("input-field");
+    input_field.id = id;
+
+    const input = document.createElement("input")
+    input.type = "text";
+    input.value = value;
+
+    const label = document.createElement("label");
+    label.for = id;
+    label.textContent = id;
+
+    input_field.appendChild(input);
+    input_field.appendChild(label);
+
+    return input_field;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,7 +23,12 @@ document.addEventListener('DOMContentLoaded', () => {
     chrome.storage.sync.get(null, (items) => {
 	for(const target of items.targets){
 	    console.log("target:", target);
-	    props_field.appendChild(create_prop_field(target));
+	    props_field.appendChild(create_form("query", target.query));
+	    props_field.appendChild(create_form("title", target.title.query));
+	    props_field.appendChild(create_form("body", target.body.query));
+	    props_field.appendChild(create_form("icon", target.icon.query));
+	    props_field.appendChild(create_form("image", target.image.query));
+	    props_field.appendChild(document.createElement("br"));
 	}
     });
 });
